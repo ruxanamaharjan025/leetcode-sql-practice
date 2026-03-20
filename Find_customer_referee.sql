5952fae35843707eaa3fc4c85cea674b7504ec9d
Ruxana Maharjan <ruxana.maharjan025@gmail.com>
2026-03-20 18:39:33 +0000

    Create Find_customer_referee.sql with solution
    Add SQL solution for finding customer referees

---
+++ Find_customer_referee.sql
@@ -0,0 +1,8 @@
+Problem: Find Customer Referee
+Difficulty: Easy
+Platform: LeetCode
+Link: https://leetcode.com/problems/find-customer-referee/description/
+Solution: 
+SELECT name 
+FROM Customer 
+WHERE referee_id != 2 OR referee_id IS NULL;
